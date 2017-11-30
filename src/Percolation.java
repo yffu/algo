3d148ed65b23158d76b4cf303ca21d7e26b082dc
yuan <YuanFang.Fu@cns-inc.com>
2017-11-30 14:12:20 -0500

    corrections made based on grader_output_1130.txt

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,27 +1,28 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	private int[] op;
-	private int sz;
-	private int n;
-	private WeightedQuickUnionUF uf;
-	private int op_cnt = 0;
-	
-	private int xyTo1D(int row, int col) {
-		if (col < 1 || col > n) throw new java.lang.IllegalArgumentException("col out of bounds");
-		if (row < 1 || row > n) throw new java.lang.IllegalArgumentException("row out of bounds");
-		return (row-1)*n + (col-1); 
-	}
+	private boolean[] op;
+	private final int n;
+	private final WeightedQuickUnionUF uf;
+	private int opCnt = 0;
+
 	// create n-by-n grid, with all sites blocked	
 	public Percolation(int n)   {
 		// default value is 0, corresponding to blocked.
-		if (n <= 0) throw new java.lang.IllegalArgumentException("n out of bounds");
-		this.n = n;
-		sz = n * n;
-		op = new int[sz];
+		if(n<=0) throw new java.lang.IllegalArgumentException("n out of bounds");
+		this.n=n;
+		int sz=n * n;
+		op=new boolean[sz];
 		// extra 2 spots are for virtual top and bottom spots
-		uf = new WeightedQuickUnionUF(sz + 2);
+		uf=new WeightedQuickUnionUF(sz + 2);
 	}
+	   
+    private int xyTo1D(int row, int col) {
+        if (col < 1 || col > n) throw new java.lang.IllegalArgumentException("col out of bounds");
+        if (row < 1 || row > n) throw new java.lang.IllegalArgumentException("row out of bounds");
+        return (row-1)*n + (col-1); 
+    }
+    
 	// open site (row, col) if it is not open already
 	public    void open(int row, int col)    {
 		// check if already open
@@ -29,31 +30,31 @@ public class Percolation {
 		// union with positions on top, bottom, left and right. 
 		int pos1D=xyTo1D(row, col); // precondition: 1 <= col <= n;  1<= row <= n otherwise exception is thrown.
 		// precondition: spot is not open -> open the spot
-		op[pos1D]=1;
+		op[pos1D]=true;
 		// increase open counter
-		op_cnt += 1;
+		opCnt+=1;
 		// left 
-		if (col > 1) {
+		if(col > 1) {
 			if (isOpen(row, col-1)) uf.union(pos1D, xyTo1D(row, col-1)); 
 		}
 		// right
-		if (col < n) {
+		if(col < n) {
 			if (isOpen(row, col+1)) uf.union(pos1D, xyTo1D(row, col+1));
 		}
 		// top 
-		if (row > 1) {
+		if(row > 1) {
 			if (isOpen(row-1, col)) uf.union(pos1D, xyTo1D(row-1, col));
 		}
 		else uf.union(pos1D, n*n); // when row = 1, connect it to virtual top spot (assumed open always)
 		// bottom
-		if (row < n) {
+		if(row < n) {
 			if(isOpen(row+1, col)) uf.union(pos1D, xyTo1D(row+1, col));
 		}
 		else uf.union(pos1D, n*n+1); // when row = n, connect it to virtual bottom spot (assumed open always)
 	}
 	// is site (row, col) open?
 	public boolean isOpen(int row, int col)  {
-		return op[xyTo1D(row, col)]==1;
+		return op[xyTo1D(row, col)];
 	}
 	// is site (row, col) full?
 	public boolean isFull(int row, int col)  {
@@ -63,7 +64,7 @@ public class Percolation {
 	}
 	// number of open sites
 	public     int numberOfOpenSites()       {
-		return op_cnt;
+		return opCnt;
 	}
 	public boolean percolates() {
 		return uf.connected(n*n, n*n+1);
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -4,29 +4,22 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
 	
-	private int[] ops;
-	private Percolation perc;
-	private int n;
-	private int trs;
-	private double op_mean;
-	private double op_stddev;
-	private double[] op_conf;
+	private final double opMean;
+	private final double opStddev;
+	private final double[] opConf;
 
-	public PercolationStats(int num, int trials) {
+	public PercolationStats(int n, int trs) {
 		// perform trials independent experiments on an n-by-n grid
-		
-		this.n=num;
-		this.trs = trials;
 		
 		if (n < 1) throw new java.lang.IllegalArgumentException("n out of bounds");
 		if (trs < 1) throw new java.lang.IllegalArgumentException("T out of bounds");
 		
-		ops = new int[trs];
-		for(int t = 0; t < trs; t++) {
+		int[] ops=new int[trs];
+		for(int t=0; t < trs; t++) {
 			// Initialize all sites to be blocked.
-			perc = new Percolation(n);
+			Percolation perc = new Percolation(n);
 			// Repeat the following until the system percolates:
-			while (!perc.percolates()){
+			while(!perc.percolates()){
 				// Choose a site uniformly at random among all blocked sites.
 				// Open the site.
 				perc.open(StdRandom.uniform(n) + 1, StdRandom.uniform(n) + 1);
@@ -34,46 +27,46 @@ public class PercolationStats {
 			ops[t]=perc.numberOfOpenSites();
 		}
 		
-		op_mean = StdStats.mean(ops)/(n*n);
-		op_stddev = StdStats.stddev(ops)/(n*n);
-		op_conf = new double[2];
-		if (Double.isNaN(op_stddev)) {
-			op_conf[0]=Double.NaN;
-			op_conf[1]=Double.NaN;
+		opMean=StdStats.mean(ops)/(n*n);
+		opStddev=StdStats.stddev(ops)/(n*n);
+		opConf=new double[2];
+		double CONFIDENCE_95=1.96;
+		if (Double.isNaN(opStddev)) {
+			opConf[0]=Double.NaN;
+			opConf[1]=Double.NaN;
 		}
 		else {
-			op_conf[0]=op_mean - (1.96 * op_stddev)/Math.sqrt(trs);
-			op_conf[1]=op_mean + (1.96 * op_stddev)/Math.sqrt(trs);
+			opConf[0]=opMean - (CONFIDENCE_95 * opStddev)/Math.sqrt(trs);
+			opConf[1]=opMean + (CONFIDENCE_95 * opStddev)/Math.sqrt(trs);
 		}
 	}
 	public double mean() {
 		// sample mean of percolation threshold
-		return op_mean;
+		return opMean;
 	}
 	public double stddev() {
 		// sample standard deviation of percolation threshold
 		// if trials equals 1? The sample standard deviation is undefined. We recommend returning Double.NaN. - covered in StdStats.stddev
-		return op_stddev;
+		return opStddev;
 	}
 	public double confidenceLo() {
 		// low  endpoint of 95% confidence interval
-		return op_conf[0];
+		return opConf[0];
 	}
 	public double confidenceHi() {
 		// high endpoint of 95% confidence interval
-		return op_conf[1];
+		return opConf[1];
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
-			PercolationStats perc_st = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-			StdOut.println("mean\t\t\t = " + perc_st.mean());
-			StdOut.println("stddev\t\t\t = " + perc_st.stddev());
-			StdOut.println("95% confidence interval\t = " + "[" + perc_st.confidenceLo() + ", " + perc_st.confidenceHi() + "]");
+			PercolationStats percSt = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+			StdOut.println("mean\t\t\t = " + percSt.mean());
+			StdOut.println("stddev\t\t\t = " + percSt.stddev());
+			StdOut.println("95% confidence interval\t = " + "[" + percSt.confidenceLo() + ", " + percSt.confidenceHi() + "]");
 		}
-		catch (NumberFormatException nfe) {
+		catch(NumberFormatException nfe){
 			System.out.println("invalid inputs " + nfe.getMessage());
-			System.exit(1);
 		}
 		
 	}
